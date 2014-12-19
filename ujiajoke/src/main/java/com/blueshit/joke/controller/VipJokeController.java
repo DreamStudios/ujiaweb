4367e9cbfec1112d5fbbdd461a8a971f05fa8dd3
zhaohuaan <zhaohuaan@163.com>
2014-12-19 08:58:20 +0800

    会员专享初步

---
+++ ujiajoke/src/main/java/com/blueshit/joke/controller/VipJokeController.java
@@ -0,0 +1,26 @@
+package com.blueshit.joke.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 会员专享模块controller
+ * @author 9527
+ * @version 1.0
+ * @email zhaohuaan0925@163.com
+ * @created 2014/12/19 8:54
+ * @description
+ */
+public class VipJokeController {
+
+
+    /**
+     * 会员专享模块
+     * @return
+     */
+    @RequestMapping(value = "/membership", method = RequestMethod.GET)
+    public String memberShip(Model model, String page){
+        return null;
+    }
+}
