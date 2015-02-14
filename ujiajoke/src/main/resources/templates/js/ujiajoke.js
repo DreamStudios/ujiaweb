2e75588545e127c8fa08b62cae95271180b38424
zhaohuaan <zhaohuaan@163.com>
2015-02-14 11:14:16 +0800

    修改轮播BUG

--- ujiajoke/src/main/resources/templates/js/ujiajoke.js
+++ ujiajoke/src/main/resources/templates/js/ujiajoke.js
@@ -4518,6 +4518,10 @@ $(function(){
 	if(parseInt($.cookie('MH_USER_ID'))>0){
 		$('#view-user-show-ajax').hide();
 	}
+    //列表页图片滚动推荐
+    if($('#listTopFocus').length>0){
+        listTopFocus();
+    }
 })
 
 /*==========================================
@@ -4528,6 +4532,67 @@ $(function(){
 		h : 弹出框的高度 如：400；（不需要加单位px）
 		t : 所操作的对象 如：'#login' / '.login'
 */
+//列表页图片滚动推荐
+function listTopFocus(){
+    var sWidth = $("#listTopFocus").width(); //获取焦点图的宽度（显示面积）
+    var len = $("#listTopFocus ul li").length; //获取焦点图个数
+    var index = 0;
+    var picTimer;
+
+    //以下代码添加数字按钮和按钮后的半透明条，还有上一页、下一页两个按钮
+    var btn = "<div class='btn'><span class='preNext pre'></span>";
+    for(var i=0; i < len; i++) {
+        btn += "<span class='point'></span>";
+    }
+    btn += "<span class='preNext next'></span></div>";
+    $("#listTopFocus").append(btn);
+
+    //为小按钮添加鼠标滑入事件，以显示相应的内容
+    $("#listTopFocus .point").mouseover(function() {
+        index = $("#listTopFocus .btn .point").index(this);
+        showPics(index);
+    }).eq(0).trigger("mouseover");
+
+    //上一页按钮
+    $("#listTopFocus .pre").click(function() {
+        index -= 1;
+        if(index == -1) {index = len - 1;}
+        showPics(index);
+    });
+
+    //下一页按钮
+    $("#listTopFocus .next").click(function() {
+        index += 1;
+        if(index == len) {index = 0;}
+        showPics(index);
+    });
+
+    //本例为左右滚动，即所有li元素都是在同一排向左浮动，所以这里需要计算出外围ul元素的宽度
+    $("#listTopFocus ul").css("width",sWidth * (len));
+
+    //鼠标滑上焦点图时停止自动播放，滑出时开始自动播放
+    $("#listTopFocus").hover(function() {
+        clearInterval(picTimer);
+    },function() {
+        picTimer = setInterval(function() {
+            showPics(index);
+            index++;
+            if(index == len) {index = 0;}
+        },4000); //此4000代表自动播放的间隔，单位：毫秒
+    }).trigger("mouseleave");
+
+    //显示图片函数，根据接收的index值显示相应的内容
+    function showPics(index) { //普通切换
+        var nowLeft = -index*sWidth; //根据index值计算ul元素的left值
+        $("#listTopFocus ul").stop(true,false).animate({"left":nowLeft},800); //通过animate()调整ul元素滚动到计算出的position
+        $("#listTopFocus .btn .point").removeClass("on").eq(index).addClass("on"); //为当前的按钮切换到选中的效果
+    }
+    $('#listTopFocus').find('a').find('img').hover(function(){
+        $(this).css('filter','alpha(opacity=70)').css('-moz-opacity','0.7').css('opacity','0.7');
+    },function(){
+        $(this).css('filter','alpha(opacity=100)').css('-moz-opacity','1').css('opacity','1');
+    })
+}
 function yojiastyleDialog(w,h,t){
 	$(t).dialog({
 		resizable:false,
